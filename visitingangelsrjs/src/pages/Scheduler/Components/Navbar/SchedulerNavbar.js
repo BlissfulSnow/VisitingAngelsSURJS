25f4b94f9ee7ecb4a054e70346fc15b8cc24ebae
Pham Chi <chi.b.pham001@gmail.com>
2025-05-08 20:17:18 -0700

    Fixed overflowing navbar and formatted caregiver/availability-card

--- visitingangelsrjs/src/pages/Scheduler/Components/Navbar/SchedulerNavbar.js
+++ visitingangelsrjs/src/pages/Scheduler/Components/Navbar/SchedulerNavbar.js
@@ -15,7 +15,7 @@ function SchedulerNavbar() {
 	return (
 		<nav className="navbar">
 				<img src="https://i.imgur.com/GSKsNA8.png" alt="logo"></img>
-			<div>
+			<div className="navbar-links">
 				<Link to="/scheduler/find-caregiver" className="navbar-link">Find Caregiver</Link>
 				<Link to="/scheduler/load-data" className="navbar-link-center">Load Data</Link>
 				<Link to="/scheduler/availability" className="navbar-link">Availability</Link>
