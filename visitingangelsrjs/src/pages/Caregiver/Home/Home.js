37a86051e227b75adcdb9c0a0ad7e046ed182598
Anna Muller <annaymuller99@gmail.com>
2025-05-12 12:43:19 -0700

    Updates dependencies

--- visitingangelsrjs/src/pages/Caregiver/Home/Home.js
+++ visitingangelsrjs/src/pages/Caregiver/Home/Home.js
@@ -59,49 +59,6 @@ function Home() {
 		document.title = "Home | SmartScheduler";
 	}, [baseUrl]);
 
-    // useEffect(() => {
-    //     function to12Hour(time24) {
-    //         const [hStr, m] = time24.split(':');
-    //         let h = parseInt(hStr, 10);
-    //         const suffix = h >= 12 ? 'pm' : 'am';
-    //         h = h % 12 || 12;           // map 0→12, 13→1, 12→12
-    //         return `${h}:${m}${suffix}`;
-	// 	    }
-    //
-    //     const fetchAvailabilities = async () => {
-    //         const token = localStorage.getItem('token');
-    //         if (!token) return setError('Not logged in');
-    //         const { username } = jwtDecode(token);
-    //
-    //         const response = await fetch(`${baseUrl}/api/db/filtered-availabilities/${username}`);
-    //         if (!response.ok) throw new Error(`HTTP Status: ${response.status}`);
-    //         let data = await response.json();
-    //         data = data.availabilities;
-	//
-    //         for (let i = 0; i < data.length; i++) {
-    //             let date = data[i].available_date;
-    //             date = date.split("T")[0];
-    //             data[i].available_date = date;
-	//
-    //             let startTime = data[i].start_time;
-    //             startTime = startTime.slice(0, startTime.length - 3);
-    //             data[i].start_time = to12Hour(startTime);
-	//
-    //             let endTime = data[i].end_time;
-    //             endTime = endTime.slice(0, endTime.length - 3);
-    //             data[i].end_time = to12Hour(endTime);
-    //         }
-    //         setAvailabilityList(data);
-    //         } catch (e) {
-    //             console.error(e);
-    //             setError('Failed to fetch your availabilities');
-    //         }
-    //     };
-	//
-    //     fetchAvailabilities();
-    //     document.title = "Home | SmartScheduler";
-    // }, [baseUrl]);
-
     async function handleDelete(id) {
         try {
             await fetch(`${baseUrl}/api/db/availability/${id}`, { method: 'DELETE' });
@@ -131,8 +88,8 @@ function Home() {
                                 <img
                                     onClick={() => handleDelete(availability.id)}
                                     className="delete-option"
-                                    src="https://cdn3.iconfinder.com/data/icons/linecons-free-vector-icons-pack/32/trash-512.png"
-                                    alt="trash"
+									src={"https://i.imgur.com/YilbdXD.png"}
+									alt="trash"
                                 />
                             </span>
                         </li>
